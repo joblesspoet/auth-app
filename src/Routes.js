30628dddda002b8b412e63849da934a1414a7018
Ghulam Rasool <sanglavi@hotmail.com>
2020-10-15 02:09:51 +0500

    added auth routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,33 +2,44 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route, Redirect
   } from "react-router-dom";
+
 import Login from './components/Auth/Login/Login';
 import ResetPassword from './components/Auth/ResetPassword/ResetPassword';
 import Signup from './components/Auth/Signup/Signup';
-
+import PageNotFound from './components/404/PageNotFound';
 import Home from './components/Home/Home';
 
 function Routes() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/" >
-                    <Home />
-                </Route>
-                <Route exact path="/auth/login">
-                    <Login />
-                </Route>
-                <Route exact path="/auth/register">
-                    <Signup />
-                </Route>
-                <Route exact path="/auth/reset-password">
-                    <ResetPassword />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/auth/login" component={Login} />
+                <Route exact path="/auth/register" component={Signup} />
+                {/* <Route exact path="/auth/reset-password" component={ResetPassword} /> */}
+                <PrivateRoute path="/auth/reset-password" component={ResetPassword} />
+                <Route isAuth path="*">
+                    <PageNotFound />
                 </Route>
             </Switch>   
         </Router>
     )
+}
+
+function PrivateRoute({ component: Component, ...rest }) {
+    return (<Route {...rest} render={ props => (    
+        rest.isAuth === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{
+            pathname: '/auth/login',
+            state: { from: props.location }
+            }}
+          />
+        )
+      )} />)
 }
 
 export default Routes
